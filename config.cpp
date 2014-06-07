130d24ce450a368c409dab264870e97d4f48639d
Nikolay Zapolnov <zapolnov@gmail.com>
2014-06-08 00:20:18 +0400

    Added the 'sound' submodule.

--- config.cpp
+++ config.cpp
@@ -100,6 +100,7 @@ Config::Config()
 	repos.insert(std::make_pair("parse-com", "https://github.com/yiptool/parse-com.git"));
 	repos.insert(std::make_pair("resources", "https://github.com/yiptool/resources.git"));
 	repos.insert(std::make_pair("scenegraph", "https://github.com/yiptool/scenegraph.git"));
+	repos.insert(std::make_pair("sound", "https://github.com/yiptool/sound.git"));
 	repos.insert(std::make_pair("stb_image_cxx", "https://github.com/yiptool/stb_image_cxx.git"));
 	repos.insert(std::make_pair("tinyxml-util", "https://github.com/yiptool/tinyxml-util.git"));
 
